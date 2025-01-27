095604564bb6767d2b6374500b3a3cbd4fde4e26
Mauritz Marais <mauritz303@gmail.com>
2025-01-27 11:17:03 +0200

    added check for json response

--- caching_proxy.py
+++ caching_proxy.py
@@ -4,6 +4,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from functools import cached_property
 from urllib.parse import parse_qsl, urlparse
 import json
+import requests
 
 cache = {}
 origin = ''
@@ -18,23 +19,40 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url.query))
     
     def get_response(self):
-        if self.url.path in cache:
-            return json.dumps (
+        try: 
+            if self.url.path in cache:
+                print("[Cache]: HIT")
+                return json.dumps (
+                    {
+                        "response": cache[self.url.path]
+                    }
+                )
+
+            print("[Cache]: MISS")        
+            # make request to url + path
+            # cache response
+            headers = {'content-type': 'application/json; charset=utf8'}
+            response = requests.get(f'{origin}{self.url.path}', headers=headers)
+            print(response.headers['Content-Type'])
+            
+            # if response content is json
+            if 'application/json' in response.headers['content-type']:
+                cache[self.url.path] = response.json()
+                return json.dumps(
                 {
-                    "response": cache[self.url.path]
+                    "response": response.json()
                 }
             )
-        
-        # make request to url/ + path
-        # cache response
-        
-        cache[self.url.path] = 'from cache'
 
-        return json.dumps(
-            {
-                "response": 'from origin'
-            }
-        )
+            cache[self.url.path] = 'NON JSON response'
+
+            return json.dumps(
+                {
+                    "response": 'NON JSON response'
+                }
+            )
+        except:
+            print("An error occured")
 
     def do_GET(self):
         self.send_response(200)
@@ -46,19 +64,19 @@ class WebRequestHandler(BaseHTTPRequestHandler):
 if __name__ == "__main__":
     error_msg = 'Enter --port <number> --origin <url>'
     
-    if(len(sys.argv) != 5):
+    if len(sys.argv) != 5:
         sys.exit(error_msg)
 
-    if(sys.argv[1] != '--port'):
+    if sys.argv[1] != '--port':
         sys.exit(error_msg)
 
-    if(sys.argv[3] != '--origin'):
+    if sys.argv[3] != '--origin':
         sys.exit(error_msg)
     
     port = int(sys.argv[2])
-    origin = sys.argv[4]
-    headers = {"accept": "application/json"}
 
+    origin = sys.argv[4].rstrip('/')
+   
     server = HTTPServer(('localhost', port), WebRequestHandler)
     print(f'Starting server on port {port}')
     server.serve_forever()
